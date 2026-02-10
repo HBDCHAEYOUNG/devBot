737d4f1a0ba8a75dee62c151b5e43910dd8f8d69
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-02-10 16:59:01 +0900

    feat : svg 컴포넌트화 라이브러리 설치 및 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [{ source: "/favicon.ico", destination: "/icon.svg" }];
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
