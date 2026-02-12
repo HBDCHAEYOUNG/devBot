c5fcdf7816a59807d0a317b84ea77e46a62d230b
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-02-12 15:19:02 +0900

    feat : 새글 작성 링크 추가

--- src/widgets/AppSidebar/ui/AppSidebar.tsx
+++ src/widgets/AppSidebar/ui/AppSidebar.tsx
@@ -10,6 +10,7 @@ import MainIcon from "@/icons/logo.svg";
 import CloseIcon from "@/icons/close.svg";
 import Link from "next/link";
 import { DocumentsList } from "@/entities/document";
+import DocumentIcon from "@/icons/document-add.svg";
 
 export function AppSidebar() {
   const { setOpen, setOpenMobile, isMobile } = useSidebar();
@@ -19,13 +20,22 @@ export function AppSidebar() {
   };
   return (
     <Sidebar>
-      <SidebarHeader className="px-4 flex flex-row justify-between">
+      <SidebarHeader className="px-3 flex flex-row justify-between">
         <Link href="/" className="cursor-pointer" onClick={handleClose}>
-          <MainIcon className="size-7 cursor-pointer" />
+          <MainIcon className="size-6 cursor-pointer" />
         </Link>
-        <CloseIcon className="size-6 cursor-pointer" onClick={handleClose} />
+        <CloseIcon className="size-5 cursor-pointer" onClick={handleClose} />
       </SidebarHeader>
-      <SidebarContent className="px-2">
+      <SidebarContent>
+        <Link
+          href="/"
+          className="flex items-center gap-2 cursor-pointer transition-colors relative hover:bg-gray-200 rounded-md p-2 text-sm my-6"
+          onClick={handleClose}
+        >
+          <DocumentIcon className="size-4" />
+          새글 작성
+        </Link>
+        <h4 className="text-sm text-gray-600 px-2">내 문서</h4>
         <DocumentsList />
       </SidebarContent>
     </Sidebar>
