a007f78a9428f5df3972f500a4ca016fc9e4763a
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-02-25 12:31:31 +0900

    style : meta설명 ui 수정

--- src/shared/ui/SeoSummaryAccordion/SeoSummaryAccordion.tsx
+++ src/shared/ui/SeoSummaryAccordion/SeoSummaryAccordion.tsx
@@ -20,25 +20,28 @@ export function SeoSummaryAccordion({
   onMetaDescriptionChange,
 }: SeoSummaryAccordionProps) {
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="seo-summary" className="bg-gray-100 rounded-sm">
-        <AccordionTrigger className="cursor-pointer bg-gray-200 rounded-sm px-2 py-3">
+    <Accordion type="single" collapsible className="w-full pt-4">
+      <AccordionItem
+        value="seo-summary"
+        className="border-x border-border rounded-sm"
+      >
+        <AccordionTrigger className="cursor-pointer border-y border-border rounded-sm! px-3 py-3">
           {mode === "view"
-            ? "생성된 SEO 메타 설명 확인하기"
+            ? "추천 SEO 메타 설명 확인하기"
             : "SEO 메타 설명 수정하기"}
         </AccordionTrigger>
-        <AccordionContent className="flex flex-col gap-6 px-2 py-4">
+        <AccordionContent className="border-b border-border rounded-sm flex flex-col gap-4 py-2">
           <Textarea
             value={metaDescription}
             readOnly={mode === "view"}
             onChange={(e) => onMetaDescriptionChange?.(e.target.value)}
-            className="w-full text-sm rounded px-2 py-2 outline-none border-none shadow-none focus-visible:ring-0 focus-visible:ring-offset-0 resize-none"
+            className="w-full text-sm rounded outline-none border-none shadow-none focus-visible:ring-0 focus-visible:ring-offset-0 resize-none"
             placeholder="SEO 메타 설명"
           />
           {mode === "view" && (
-            <p className="text-green-800 text-xs">
-              HTML 다운로드 시 meta 태그의 content 안에 들어가며, 검색 결과·링크
-              공유 미리보기에 사용됩니다.
+            <p className="text-rose-400 text-xs px-3 pb-2">
+              ✔︎ HTML 다운로드 시 meta 태그의 content 안에 들어가며, 검색
+              결과·링크 공유 미리보기에 사용됩니다.
             </p>
           )}
         </AccordionContent>
